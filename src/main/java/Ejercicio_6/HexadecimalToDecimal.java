c35f298108ce42bc0d99e3c98bd309a275598f55
ZoeAcero <zar.amazona@gmail.com>
2024-04-19 11:10:38 +0200

    a

--- src/main/java/Ejercicio_6/HexadecimalToDecimal.java
+++ src/main/java/Ejercicio_6/HexadecimalToDecimal.java
@@ -6,4 +6,7 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 public class HexadecimalToDecimal {
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(() -> createAndShowGUI());
+    }
 }
