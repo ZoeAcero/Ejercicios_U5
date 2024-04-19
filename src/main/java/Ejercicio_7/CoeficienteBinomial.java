0b2e9d0df3df7c5ebad8a4482491c8dceadea21e
ZoeAcero <zar.amazona@gmail.com>
2024-04-19 11:15:53 +0200

    a

--- src/main/java/Ejercicio_7/CoeficienteBinomial.java
+++ src/main/java/Ejercicio_7/CoeficienteBinomial.java
@@ -36,4 +36,18 @@ public class CoeficienteBinomial {
 
         frame.setVisible(true);
     }
+
+    public static int contarGenes(String cadenaADN) {
+        int contador = 0;
+        int indice = 0;
+        while (indice < cadenaADN.length()) {
+            indice = cadenaADN.indexOf("ATG", indice);
+            if (indice == -1) {
+                break;
+            }
+            contador++;
+            indice += 3;
+        }
+        return contador;
+    }
 }
