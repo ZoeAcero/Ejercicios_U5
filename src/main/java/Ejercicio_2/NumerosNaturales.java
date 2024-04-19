08fd3d7d38763d5fc4dbf4fcf983c025c700bb6e
ZoeAcero <zar.amazona@gmail.com>
2024-04-19 10:50:42 +0200

    a

--- src/main/java/Ejercicio_2/NumerosNaturales.java
+++ src/main/java/Ejercicio_2/NumerosNaturales.java
@@ -2,5 +2,16 @@ package Ejercicio_2;
 
 public class NumerosNaturales {
 
-    
+    public static void main(String[] args) {
+        int a = 3;
+        int d = 8;
+        imprimirNaturalesEntre(a, d);
+    }
+
+    public static void imprimirNaturalesEntre(int a, int d) {
+        for (int i = a; i <= d; i++) {
+            System.out.println(i);
+        }
+    }
 }
+
