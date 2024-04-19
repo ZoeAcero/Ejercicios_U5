30f0bc8f26686d1d221f9eb6246add1a0b084592
ZoeAcero <zar.amazona@gmail.com>
2024-04-19 11:19:55 +0200

    a

--- src/main/java/Ejercicio_8/ContadorGenes.java
+++ src/main/java/Ejercicio_8/ContadorGenes.java
@@ -18,4 +18,37 @@ public class ContadorGenes {
         JTextField textField = new JTextField();
         JButton button = new JButton("Count Genes");
         JLabel label = new JLabel();
+
+        button.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String cadenaADN = textField.getText();
+                int genes = contarGenes(cadenaADN);
+                label.setText("La cadena de ADN tiene " + genes + " genes.");
+            }
+        });
+
+        frame.setLayout(new FlowLayout());
+        frame.add(new JLabel("Enter DNA sequence:"));
+        frame.add(textField);
+        frame.add(button);
+        frame.add(label);
+
+        frame.setVisible(true);
+    }
+
+    public static int contarGenes(String cadenaADN) {
+        int contador = 0;
+        int indice = 0;
+        while (indice < cadenaADN.length()) {
+            indice = cadenaADN.indexOf("ATG", indice);
+            if (indice == -1) {
+                break;
+            }
+            contador++;
+            indice += 3;
+        }
+        return contador;
+    }
 }
+
