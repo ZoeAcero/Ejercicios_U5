09804346a5f4cf8e95bc03de0b9b80cba338afae
ZoeAcero <zar.amazona@gmail.com>
2024-04-19 11:14:49 +0200

    a

--- src/main/java/Ejercicio_7/CoeficienteBinomial.java
+++ src/main/java/Ejercicio_7/CoeficienteBinomial.java
@@ -6,4 +6,12 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 public class CoeficienteBinomial {
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(() -> createAndShowGUI());
+    }
+
+    private static void createAndShowGUI() {
+        JFrame frame = new JFrame("Contador de Genes");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(300, 200);
 }
