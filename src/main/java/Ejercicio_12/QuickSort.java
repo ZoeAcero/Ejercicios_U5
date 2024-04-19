dc8331220865bbacd52fb82f1aa137aafde84366
ZoeAcero <zar.amazona@gmail.com>
2024-04-19 11:37:53 +0200

    a

--- src/main/java/Ejercicio_12/QuickSort.java
+++ src/main/java/Ejercicio_12/QuickSort.java
@@ -38,4 +38,23 @@ public class QuickSort {
 
         frame.setVisible(true);
     }
+
+    public static void quickSort(int[] arr, int low, int high) {
+        if (low < high) {
+            int pi = partition(arr, low, high);
+            quickSort(arr, low, pi - 1);
+            quickSort(arr, pi + 1, high);
+        }
+    }
+
+    public static int partition(int[] arr, int low, int high) {
+        int pivot = arr[high];
+        int i = low - 1;
+        for (int j = low; j < high; j++) {
+            if (arr[j] < pivot) {
+                i++;
+                int temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+            }
 }
