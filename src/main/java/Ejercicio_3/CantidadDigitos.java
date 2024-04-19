caf41a55b36e62ff5fcfc880adec8629ca22b398
ZoeAcero <zar.amazona@gmail.com>
2024-04-19 10:58:56 +0200

    a

--- src/main/java/Ejercicio_3/CantidadDigitos.java
+++ src/main/java/Ejercicio_3/CantidadDigitos.java
@@ -1,4 +1,14 @@
 package Ejercicio_3;
 
 public class CantidadDigitos {
+
+    public static void main(String[] args) {
+        int numero = 12345; // Ejemplo: contar los dígitos de 12345
+        int cantidadDigitos = contarDigitos(numero);
+        System.out.println("El número " + numero + " tiene " + cantidadDigitos + " dígitos.");
+    }
+
+    public static int contarDigitos(int numero) {
+        return (int) Math.log10(Math.abs(numero)) + 1;
+    }
 }
